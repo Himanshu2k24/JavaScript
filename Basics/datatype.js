5e268cea0347d5799b9e2b58c3e3bcf500d95087
Himanshu Tiwary <149292671+Himanshu2k24@users.noreply.github.com>
2025-07-20 18:08:45 +0000

    New

---
+++ Basics/datatype.js
@@ -0,0 +1,20 @@
+"use strict" 
+
+console.log("Himasnhu");
+
+let name = "Himanshu"  //String
+let age = 18 //Number
+let isLoggedin = true //Boolean
+
+
+
+//null => standalone value
+//undefined
+//Symbol => Unique
+
+// Object
+
+console.log(typeof name);
+console.log(typeof null); //Object
+
+
---
+++ Basics/variable.js
@@ -0,0 +1,15 @@
+const account_Id = 1444453                  
+let accountemail = "htiwary09@gmail.com"     //
+var accorPassword = "12345"
+accountCity = "Jaipur"
+
+let accountstate;
+
+//account_Id = 2     //Not changable
+accountemail = "a@gmail.com"
+accorPassword = "54321"
+accountCity = "Arrah"
+
+console.log(account_Id)
+
+console.table([accorPassword, accountemail , accountCity , accountstate])
